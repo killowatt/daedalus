3d64384f42d764f451d6e307776cbe27dd56fa6b
killowatt <killowatt2213@gmail.com>
2022-01-28 15:03:07 -0500

    Add SDL2

--- source/Main.cpp
+++ source/Main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include "SDL2/SDL.h"
 
-int main()
+int main(int argc, char* args[])
 {
-	std::cout << "hello\n";
+	SDL_Window* window = NULL;
+	SDL_Surface* screenSurface = NULL;
+
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) < 0)
+	{
+		return 1;
+	}
+
+	window = SDL_CreateWindow(
+		"Hello World",
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		1280, 720,
+		SDL_WINDOW_SHOWN
+	);
+
+	if (window == NULL) 
+	{
+		return 1;
+	}
+
+	SDL_Event event;
+	while (true)
+	{
+		if (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+			{
+				break;
+			}
+		}
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	return 0;
 };
